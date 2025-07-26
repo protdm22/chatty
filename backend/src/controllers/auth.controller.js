69e6a33e474cdd6c9996025b4d1d172e70120c5e
protdm22 <protdm22@gmail.com>
2025-07-26 18:40:43 +0300

    Profile page done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,7 +39,7 @@ export const signup = async (req, res) => {
         console.log("Error in signup controller", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+};
 
 export const login = async (req, res) => {
     const { email, password } = req.body;
@@ -61,7 +61,7 @@ export const login = async (req, res) => {
         console.log("Error in login controller", error);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+};
 
 export const logout = (req, res) => {
     try {
@@ -71,7 +71,7 @@ export const logout = (req, res) => {
         console.log("Error in logout controller", error);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+};
 
 export const updateProfile = async (req, res) => {
     try {
@@ -81,14 +81,18 @@ export const updateProfile = async (req, res) => {
         if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
 
         const uploadResponse = await cloudinary.uploader.upload(profilePic);
-        const updatedUser = User.findById(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
 
-        res.send(200).json(updatedUser);
+        res.status(200).json(updatedUser);
     } catch (error) {
         console.log("Error in update profile controller:", error);
         res.send(500).json({ message: "Internal server error" });
     }
-}
+};
 
 export const checkAuth = (req, res) => {
     try {
@@ -97,4 +101,4 @@ export const checkAuth = (req, res) => {
         console.log("Error in checkAuth controller:", error);
         res.status(500).json({ message: "Internal server error" });
     }
-}
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,9 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
 app.use(cookieParser());
 app.use(
     cors({
