d4892ff3442cd432fb793721374ec09236365fc4
protdm22 <protdm22@gmail.com>
2025-07-28 23:00:24 +0300

    Fix bug in auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,19 +4,19 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.jwt
-        if (!token) res.status(401).json({ message: "Unauthorized - No Token Provided" });
+        if (!token) return res.status(401).json({ message: "Unauthorized - No Token Provided" });
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) res.status(403).json({ message: "Forbidden - Invalid Token" });
+        if (!decoded) return res.status(403).json({ message: "Forbidden - Invalid Token" });
 
         const user = await User.findById(decoded.userId).select("-password");
-        if (!user) res.status(404).json({ message: "User not found" });
+        if (!user) return res.status(404).json({ message: "User not found" });
 
         req.user = user;
 
         next();
     } catch (error) {
         console.log("Error in protectRoute middleware", error);
-        res.status(500).json({ message: "Internal server error" });
+        return res.status(500).json({ message: "Internal server error" });
     }
 }
